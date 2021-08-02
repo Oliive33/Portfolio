97cd6d583ad34efe01ed30b271d18c28450fe495
olivier villevieille <oliviervillevieille@hotmail.fr>
2021-08-02 18:45:12 +0200

    minify files

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-//Loader
-const contLoader = document.querySelector(".container-loader");
-
-window.addEventListener("DOMContentLoaded", () => {
-  if (sessionStorage.getItem("premierChargement") === null) {
-    contLoader.classList.add("fondu-out");
-  } else {
-    contLoader.style.display = "none";
-  }
-
-  sessionStorage.setItem("premierChargement", "done");
-});
-
 //Menu Navigation Left
 
 const btnMenu = document.querySelector(".btn-rond-menu");
