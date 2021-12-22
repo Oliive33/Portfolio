2cc6aee5a1b906f65d8c12fce3f11f8f0b7c222b
olivier villevieille <oliviervillevieille@hotmail.fr>
2021-12-22 14:24:17 +0100

    ajout section swiper

--- app.js
+++ app.js
@@ -312,3 +312,20 @@ const clockText = () => {
   dateYear.innerHTML = year;
 };
 setInterval(clockText, 1000);
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper(".new-swiper", {
+  spaceBetween: 20,
+  loop: "true",
+  slidesPerView: "auto",
+  centeredSlides: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    992: {
+      spaceBetween: 80,
+    },
+  },
+});
